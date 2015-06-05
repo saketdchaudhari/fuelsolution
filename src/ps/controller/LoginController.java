19c6c84ecb57ee9d5e464a166a9823b52679770e
saketdchaudhari <saketdchaudhari@gmail.com>
2015-06-04 20:10:20 -0400

    Adding persistence support
    1. Added

--- src/ps/controller/LoginController.java
+++ src/ps/controller/LoginController.java
@@ -1,5 +1,9 @@
 package ps.controller;
 
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,13 +13,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LoginController {
 	
+	@Autowired
+	LocalEntityManagerFactoryBean entityManagerFactory;
+	
 	@RequestMapping(method = RequestMethod.GET, value = "/login")
-    public
+	public
     ModelAndView check(@RequestParam(value = "userName", required = true) String userName,
     		@RequestParam(value = "password", required = true) String password) {
 		ModelAndView mav = new ModelAndView("welcomeView");
+		Query query = entityManagerFactory.nativeEntityManagerFactory.createEntityManager().createNativeQuery("SELECT * FROM pump_type where pump_id = 1");
+		query.getSingleResult();
 		return mav;
 	}
-	
-	
 }
